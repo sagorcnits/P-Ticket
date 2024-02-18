44ebf70dd78cbb36da0bdc89200f815ce88cbde2
Sagor Hossain <sagor.cnits@gmail.com>
2024-02-18 19:39:18 +0600

    seat validation added

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 const seatBtn = document.querySelectorAll(".seat");
-
-// console.log(seatBtn);
-
+// limitedcounter
+let limitedTicketCount = 0;
+// convert number
 let seatLeft = convertNumber("total-seat");
 let seatCount = convertNumber("seat-count");
 let totalPrice = convertNumber("total-price");
@@ -10,34 +10,37 @@ let grandPrice = convertNumber("grand-price");
 for (let seat of seatBtn) {
   seat.addEventListener("click", function (event) {
     const targetValue = event.target;
-    if (targetValue.style.backgroundColor) {
-      //   event.target.style.backgroundColor = "#1DD100";
-      //   seatLeft += 1;
-      //   seatCount -= 1;
-      //   totalPrice -= 550;
-      //   grandPrice -= 550;
-      //   // setText call
-      //   setTextElement("total-seat", seatLeft);
-      //   setTextElement("seat-count", seatCount);
-      //   setTextElement("total-price", totalPrice);
-      //   setTextElement("grand-price", grandPrice);
-      //   // add item call
-      //    addSeatItem(event.target.innerText);
-    } else {
-      event.target.style.backgroundColor = "#1DD100";
-      seatLeft -= 1;
-      seatCount += 1;
-      totalPrice += 550;
-      grandPrice += 550;
-      // setText call
-      setTextElement("total-seat", seatLeft);
-      setTextElement("seat-count", seatCount);
-      setTextElement("total-price", totalPrice);
-      setTextElement("grand-price", grandPrice);
-      // add item call
-      addSeatItem(event.target.innerText);
+    if (!targetValue.style.backgroundColor) {
+      limitedTicketCount += 1;
+      if (limitedTicketCount > 4) {
+        console.log("sorry");
+      } else {
+        event.target.style.backgroundColor = "#1DD100";
+        updatCount();
+        addSeatItem(event.target.innerText);
+      }
     }
   });
+}
+
+// count updating function
+
+function updatCount() {
+  // counting
+  seatLeft -= 1;
+  seatCount += 1;
+  totalPrice += 550;
+  grandPrice += 550;
+  // setText call
+  setTextElement("total-seat", seatLeft);
+  setTextElement("seat-count", seatCount);
+  setTextElement("total-price", totalPrice);
+  setTextElement("grand-price", grandPrice);
+  // apply btn enable & disable
+  if (limitedTicketCount >= 4) {
+    getElementId("apply-btn").disabled = false;
+    getElementId("apply-btn").style.backgroundColor = "#1DD100";
+  }
 }
 
 // add item function
